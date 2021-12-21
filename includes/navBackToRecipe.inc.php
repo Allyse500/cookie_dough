6ae85856fe38cae4cbd43f839cea42127f143cc1
Allyse <ayubired@gmail.com>
2021-12-20 18:10:20 -0600

    prepared template for recipes public document

--- includes/navBackToRecipe.inc.php
+++ includes/navBackToRecipe.inc.php
@@ -3,16 +3,6 @@
 //if a post action was entered by the button named "messageCloseButton"...
 if(isset($_POST["messageCloseButton"])){
 
-    //make super global variables from submitted form data
-    // $title = $_POST["recipeName"];
-    // $ingredients = $_POST["recipeIngredients"];
-    // $preparation = $_POST["recipePreparation"];
-    //start session to make global variables
-    session_start();
-    //make temporary variables to help keep user's place
-    // $_SESSION["temporaryRecipeTitle"] = $title;
-    // $_SESSION["temporaryIngredients"] = $ingredients;
-    // $_SESSION["temporaryPreparation"] = $preparation;
     header("location: ../user.php?recipeBack");//pull up Recipe prompt box
     exit();
 }
